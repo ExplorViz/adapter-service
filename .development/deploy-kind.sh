f20bd5e6879f26c9c355a56d7eba108e4dc8b8f0
Alexander Krause <akr@informatik.uni-kiel.de>
2020-07-16 21:31:42 +0200

    use namespace in manifest

--- .development/deploy-kind.sh
+++ .development/deploy-kind.sh
@@ -6,4 +6,4 @@ docker build -f src/main/docker/Dockerfile.jvm -t explorviz/adapter-service-jvm 
 
 kind load docker-image explorviz/adapter-service-jvm:latest
 
-kubectl apply -f manifest.yml
+kubectl apply --namespace=explorviz-dev -f manifest.yml
