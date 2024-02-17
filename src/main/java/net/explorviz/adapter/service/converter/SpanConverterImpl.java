120d2e8b9dc44052c7bfe989ebca5ea18e6091d6
Modi2000r <stu232917@mail.uni-kiel.de>
2024-02-17 15:52:04 +0100

    Adding an if clause which changes the unknown method to the Span ID

--- src/main/java/net/explorviz/adapter/service/converter/SpanConverterImpl.java
+++ src/main/java/net/explorviz/adapter/service/converter/SpanConverterImpl.java
@@ -1,5 +1,7 @@
 package net.explorviz.adapter.service.converter;
 
+import static net.explorviz.adapter.service.converter.DefaultAttributeValues.DEFAULT_FQN;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import net.explorviz.avro.Span;
 
@@ -11,10 +13,20 @@ public class SpanConverterImpl implements SpanConverter<Span> {
 
   private static final long TO_MILLISEC_DIVISOR = 1_000_000L;
 
+
   @Override
   public Span fromOpenTelemetrySpan(final io.opentelemetry.proto.trace.v1.Span ocSpan) {
 
     final AttributesReader attributesReader = new AttributesReader(ocSpan);
+
+    String methodName = "";
+    if (attributesReader.getMethodFqn() == DEFAULT_FQN) {
+      methodName = IdHelper.converterSpanId(ocSpan.getSpanId().toByteArray()) + "." + IdHelper.converterSpanId(ocSpan.getSpanId().toByteArray())
+      + "." + IdHelper.converterSpanId(ocSpan.getSpanId().toByteArray());
+    }
+    else {
+      methodName = attributesReader.getMethodFqn();
+    }
 
     String parentSpan = "";
     if (ocSpan.getParentSpanId().size() > 0) {
@@ -26,8 +38,12 @@ public class SpanConverterImpl implements SpanConverter<Span> {
         .setParentSpanId(parentSpan)
         .setSpanId(IdHelper.converterSpanId(ocSpan.getSpanId().toByteArray()))
         .setTraceId(IdHelper.converterTraceId(ocSpan.getTraceId().toByteArray()))
+        .setFullyQualifiedOperationName(methodName)
         .setStartTimeEpochMilli(ocSpan.getStartTimeUnixNano() / TO_MILLISEC_DIVISOR)
         .setEndTimeEpochMilli(ocSpan.getEndTimeUnixNano() / TO_MILLISEC_DIVISOR);
+
+    System.out.print("SpanID: " + IdHelper.converterSpanId(ocSpan.getSpanId().toByteArray()));
+    System.out.print("FQN: " + span.getFullyQualifiedOperationName());
 
     attributesReader.appendToSpan(span);
 
